acb06962493e67d6907fd9b467b1eabbe515ef84
Shashank Mangal <127585673+Shashankmangal1@users.noreply.github.com>
2025-10-05 14:54:15 +0530

    Defined Project Structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Routes, Route } from "react-router-dom"
+import { NotFound } from "./pages/NotFound"
+
+
+
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
