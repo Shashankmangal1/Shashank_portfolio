13ae6a059842205a48843b2823ca65a9cbcc816b
Shashank Mangal <127585673+Shashankmangal1@users.noreply.github.com>
2025-10-06 17:40:19 +0530

    Edited some components

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,7 @@ export const AboutSection = () => {
                 href="https://drive.google.com/file/d/1dittDL6hqtU9Wf6wmDgp0oNdH1u3_9cN/view?usp=drivesdk"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
-                Download CV
+                View CV
               </a>
             </div>
           </div>
